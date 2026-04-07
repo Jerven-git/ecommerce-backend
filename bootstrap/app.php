2e6575e65893e2801948783fdda181f82e170ee7
jerven <latayada1233@gmail.com>
2026-04-07 15:34:50 +0800

    update app.php remove duplicate use case cookie

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -28,9 +28,6 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
 
         $middleware->api(append: [
-            \Illuminate\Cookie\Middleware\EncryptCookies::class,
-            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-            \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ]);
 
